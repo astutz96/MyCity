99a37972d52907aba98b6ea22ffe6f3ddd41e5de
austinstutzman <alsghs49@gmail.com>
2023-12-22 21:23:55 -0500

    Initial attempt at setting up the row items for the MyCityHomeScreen

--- app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
+++ app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
@@ -1,23 +1,33 @@
 package com.example.mycity
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycity.datasource.dataSource
+import com.example.mycity.model.Category
 
 
 @Composable
 fun MyCityHomeScreen() {
 
-    val cityCategories = dataSource.cityCatories;
+    val cityCategories = dataSource.cityCatories
     LazyColumn {
         items(cityCategories) {
-            Text(text = stringResource(id = it.name))
+            MyCityCategoryItem(category = it)
         }
+    }
+}
+@Composable
+fun MyCityCategoryItem(category: Category){
+    Row(){
+        Image(painter = painterResource(id = category.imageResourceId), contentDescription = null)
+        Text(text = stringResource(id = category.name))
     }
 }
 
