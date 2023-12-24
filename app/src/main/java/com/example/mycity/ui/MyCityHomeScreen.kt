4c1c83477c62b621447f9ac67eb4334551022967
austinstutzman <alsghs49@gmail.com>
2023-12-24 10:04:47 -0500

    Fixing up some spacing/padding issues with the MyCityHomeScreen.
    It looks good now, could use animations and more over-all thoughfullness but the general layout is correct now on a standard phone in portrait view.

--- app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
+++ app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
@@ -1,14 +1,26 @@
 package com.example.mycity
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycity.datasource.dataSource
 import com.example.mycity.model.Category
 
@@ -23,11 +35,23 @@ fun MyCityHomeScreen() {
         }
     }
 }
+
 @Composable
-fun MyCityCategoryItem(category: Category){
-    Row(){
-        Image(painter = painterResource(id = category.imageResourceId), contentDescription = null)
-        Text(text = stringResource(id = category.name))
+fun MyCityCategoryItem(category: Category) {
+
+    Row(modifier = Modifier
+        .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = category.imageResourceId), contentDescription = null,
+            modifier = Modifier
+                .size(90.dp)
+                .padding(8.dp),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = stringResource(id = category.name), fontFamily = FontFamily.SansSerif, fontSize = 24.sp)
     }
 }
 
